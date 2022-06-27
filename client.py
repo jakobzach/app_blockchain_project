572acae92bd79dab0a1c2c779f9c8233a8c5d732
liatoumazi <leia_touma@hotmail.com>
2022-06-27 10:16:15 +0200

    fixed crypto imports v2

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ import binascii
 ## Client class definition
 class Client:
    def __init__(self):
-      random = crypto.Random.new().read
+      random = Crypto.Random.new().read
       self._private_key = RSA.generate(1024, random)
       self._public_key = self._private_key.publickey()
       self._signer = PKCS1_v1_5.new(self._private_key)
