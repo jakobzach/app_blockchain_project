bd79f2fe18f0f7455462aee9170a10f0999b85f4
Jakob Zacherl <jakobzacherl@Jakobs-MBP.home>
2022-07-01 21:21:10 +0200

    added clear_transactions method to TransactionPool

--- transaction_pool.py
+++ transaction_pool.py
@@ -20,3 +20,9 @@ class TransactionPool:
         '''takes a transaction object as argument and appends it to transaction pool'''
         with open('transactions_file', 'ab+') as transactions_file:
             pickle.dump(transaction, transactions_file)
+    
+    def clear_transactions(self) -> None:
+        '''takes no arguments and cleans the transactions file'''
+        clearer = []
+        with open('transactions_file', 'wb') as transactions_file:
+            pickle.dump(clearer, transactions_file)
