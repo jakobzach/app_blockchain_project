14b4cdf726291116c6a8d8595613c0f83365d074
vincentstadler <vincentstadler@MBP-von-Vincent.home>
2022-07-03 18:54:49 +0200

    added trigger transaction function plus minor amends

--- wallet.py
+++ wallet.py
@@ -2,6 +2,7 @@
 from distutils.command.build import build
 from tkinter import *
 from tkinter import ttk
+from xmlrpc.client import boolean
 
 # import all project classes
 import client
@@ -11,14 +12,51 @@ import block
 import blockchain
 
 def build_transaction_history(blocks: list) -> list:
-    '''takes a list of block items and returns a list of all transaction details'''
+    '''takes a list of block objects and returns a list of all transaction details'''
     transaction_history_list = []
     for item in blocks: # iterate through all blocks
         for record in item.retrieve_block()[0]: # iterate through all transactions of a block
             transaction_info = record.get_transaction_details('trunc')
+            # filter block to find Jakob's transactions?
             transaction_history_list.append(transaction_info)
 
     return transaction_history_list
+
+def get_last_block_hash(blocks: list) -> str:
+    '''takes a list of block objects and returns a string with the last block's hash'''
+    return blocks[-1][2]
+
+def trigger_transaction(sender: str, recipient: str, amount: float) -> boolean:
+    # aggregate all clients
+    clients = [Jakob, Vincent, Lia, Costanza, miner1]
+
+    new_transaction = transactions.Transaction(Jakob.identity,Vincent.identity,float(69000)) # initiate transaction
+    
+    # identify sender using the sender's identity
+    for i in clients: 
+        if i.identity == new_transaction.sender:
+            client_obj = i
+
+    trans_signature = new_transaction.sign_transaction(client_obj) # sign transaction
+    if miner1.verify_transaction(client_obj, new_transaction,trans_signature) == True:
+        new_transaction.display_transaction()
+        pool.add_to_transaction_pool(new_transaction)
+        print('Valid transaction')
+        print(len(pool.retr_transaction_pool()))
+    else:
+        print('Invalid transaction')
+    if len(pool.retr_transaction_pool()) == 2:
+        new_block = block.Block()
+        new_block.mine_block(pool.retr_transaction_pool(),last_block_hash, miner1.mine(new_block))
+        PyCoins_chain.chain_block(new_block)
+        pool.clear_transactions()
+        miner1.change_balance(10)
+        print('transactions: '+ str(len(pool.retr_transaction_pool())) + ', miner balance: ' + str(miner1.retrieve_balance()))
+        return True
+    else:
+        print('transactions: '+ str(len(pool.retr_transaction_pool())) + ', miner balance: ' + str(miner1.retrieve_balance()))
+        return False
+
 
 
 # defining the calcualtion for the balance
@@ -40,9 +78,6 @@ if __name__ == "__main__":
     Costanza = client.Client()
     miner1 = client.Client()
 
-    # aggregate all clients
-    clients = [Jakob, Vincent, Lia, Costanza, miner1]
-
     # defining global variable for last_block hash which is needed for first block's previous_block_hash
     last_block_hash = ""
 
@@ -61,10 +96,10 @@ if __name__ == "__main__":
     amount_entry = ttk.Entry(mainframe, width=7, textvariable=amount)
     amount_entry.grid(column=3, row=5, sticky=(W, E))
 
-    # create widget to input the receiver's id
-    receiver = StringVar()
-    receiver_entry = ttk.Entry(mainframe, width=7, textvariable=receiver)
-    receiver_entry.grid(column=3, row=4, sticky=(W, E))
+    # create widget to input the recipient's id
+    recipient = StringVar()
+    recipient_entry = ttk.Entry(mainframe, width=7, textvariable=recipient)
+    recipient_entry.grid(column=3, row=4, sticky=(W, E))
 
     # create widget that displays the resulting amount in the wallet after the transaction is made 
     balance = StringVar()
@@ -90,12 +125,16 @@ if __name__ == "__main__":
     # create a listbox for transactions history
     transactions_history = StringVar()
     listbox = Listbox(mainframe, listvariable=transactions_history).grid(column=2, columnspan=3, row=8, sticky=(W, E))
-    transactions_history.set(build_transaction_history(PyCoins_chain.retr_blockchain())) #filter block to find Jakob's transactions
+    transactions_history.set(build_transaction_history(PyCoins_chain.retr_blockchain()))
+
+    # record a new transaction
+    if trigger_transaction(Jakob.identity,recipient,float(amount)) == True:
+        transactions_history.set(build_transaction_history(PyCoins_chain.retr_blockchain()))
 
     for child in mainframe.winfo_children(): 
         child.grid_configure(padx=5, pady=5)
 
-    receiver_entry.focus() # the cursor will start in that field, so users don't have to click on it before starting to type
+    recipient_entry.focus() # the cursor will start in that field, so users don't have to click on it before starting to type
     root.bind("<Return>", calculate) # if a user presses the Return key it should call the "send" command
 
     root.mainloop() # necessary for everything to appear onscreen and allow users to interact with it
