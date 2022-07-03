09f8ded87650cbbb87b0085c1c95ef9aaff56671
Jakob Zacherl <jakobzacherl@Jakobs-MBP.home>
2022-07-03 19:22:56 +0200

    created a file to generate the genesis transaction and tested it successfully

---
+++ genesis.py
@@ -0,0 +1,21 @@
+# import all project classes
+import client
+import transactions
+import transaction_pool
+import blockchain
+
+# this file generates the genesis transaction
+
+# instantiate the necessary objects
+PyCoins_chain = blockchain.Blockchain()
+pool = transaction_pool.TransactionPool()
+Jakob = client.Client()
+
+pool.clear_transactions()
+PyCoins_chain.reset_blockchain()
+
+first_transaction = transactions.Transaction("Genesis",Jakob.identity,float(69000))
+pool.add_to_transaction_pool(first_transaction)
+
+print(pool.retr_transaction_pool())
+print(PyCoins_chain.retr_blockchain())
