e41fe18dc5ea2b972c073470f1305e9163fc6968
Jakob Zacherl <jakobzacherl@Jakobs-MBP.home>
2022-07-10 09:31:08 +0200

    cleaned up and commented block class

--- block.py
+++ block.py
@@ -1,18 +1,18 @@
 class Block:
-    def __init__(self):
-        self.__verified_transactions = []
-        self.__previous_block_hash = ""
-        self.__Nonce = ""
+    def __init__(self) -> None:
+        '''takes no inputs and does not return anything'''
+        self.__verified_transactions = [] # list of transactions the miner verified
+        self.__previous_block_hash = "" # for traceability between blocks
+        self.__Nonce = "" # the miner's proof of work
 
     # mutator – mining method
-    def mine_block(self, transactions: list, hash, nonce) -> None:
+    def mine_block(self, transactions: list, hash: str, nonce: str) -> None:
+        '''takes a list of transactions, a hash string and a nonce string, does not return anything'''
         self.__verified_transactions = transactions
         self.__previous_block_hash = hash
         self.__Nonce = nonce
     
     # accessor
     def retrieve_block(self):
+        '''takes no inputs and returns in this order: list of block's transactions, previous block's hash and this block's Nonce'''
         return self.__verified_transactions, self.__previous_block_hash, self.__Nonce
-
-    # def __str__(self) -> str:
-    #     return str(self.previous_block_hash) + '\n' + str(self.Nonce) + '\n' + str(self.verified_transactions)
